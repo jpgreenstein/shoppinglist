46bd76c2cd12d6967b7f85893f5433e810099ebe
jpgreenstein <justin.greenstein@saltermitchell.com>
2016-07-21 23:47:01 -0400

    added modal and ability to delete list item

--- js/app.js
+++ js/app.js
@@ -2,5 +2,10 @@ $(document).ready(function() {
 	$('.check').mousedown(function() {	
 	  console.log('mousedown');
 	  $('.item-description').toggleClass('item-strike');
-  })
+  	})
+	$('.close').click(function(){
+	  console.log('delete pressed');
+	  $('.item-container').remove();
+	})
+
 });
