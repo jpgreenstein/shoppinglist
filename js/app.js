8e297cd7d84b36d1f4e75cd5a6fe6751ceb5bc37
jpgreenstein <justin.greenstein@saltermitchell.com>
2016-08-16 01:48:43 -0400

    closer but not close enough

--- js/app.js
+++ js/app.js
@@ -9,11 +9,20 @@ $(document).ready(function() {
 	})
 
 	$("#addItem").click(function () {
-		console.log('button clicked');
 		var listItem = document.getElementById("add").value;
-		document.getElementById("description").innerHTML = listItem;
-		$('ul').show();
-		$('.list').appendChild(listItem);
+			console.log(listItem); 
+		if (listItem < 0) {
+			console.log('hello');
+		}
+			/* document.getElementById("description").innerHTML = listItem;
+
+			$('ul').show();
+			
+			var listedItems = document.createElement("new-item")
+
+			$('#list').append('<li class="col-xs-12 item-container" id="new-item"><div class="col-xs-2"><a href="#" class="btn btn-lg check">got it</a></div><div class="col-xs-8 item-description"><p id="description"></p></div><div class="col-xs-2"><a href="#" class="btn btn-lg delete" data-toggle="modal" data-target="#myModal">×</a></div></li>')
+			document.getElementById("description").innerHTML = listItem;
+			console.log($('#list'));*/
 	})
 
 });
